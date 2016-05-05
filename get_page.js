ddf85f6bfb91ddc606d211d50fca65b8e3652c2a
Saad Bakht <rotk13@gmail.com>
2016-05-04 22:09:11 -0400

    scrape and create files

--- get_page.js
+++ get_page.js
@@ -1,9 +1,49 @@
 var request = require('request');
+var cheerio = require('cheerio');
+var Task = require("data.task");
+var fs = require('fs');
+var _ = require('ramda');
 
-// request('http://www.google.com', function (error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     console.log(body) // Show the HTML for the Google homepage.
-//   }
-// })
+// TODO - legal headers and timeout so don't get banned
+// TODO - dies on non self posts
 
-request.get('https://www.reddit.com/r/leagueoflegends/comments/4hvlvj/singeds_mega_adhesive_should_ground/').pipe(process.stdout);
+function downloadUrl(url) {
+	request('https://www.reddit.com' + url, function (error, response, body) {
+		if(!error && response.statusCode == 200) {
+			$ = cheerio.load(body);
+			console.log($('a.title').html());
+			console.log($('#siteTable .usertext-body p').html());
+		}
+	}).pipe(fs.createWriteStream(getId(url)));
+}
+
+function getId(str) {
+	var postId = str.match(/comments\/(\w+)\/*/)[1];
+	console.log(postId);
+	return postId;
+}
+
+
+// TODO - make async
+function getInfoFromFile() {
+	$ = cheerio.load(fs.readFileSync("file2.txt"));
+	console.log($('a.title').html());
+	console.log($('#siteTable .usertext-body p').html());
+}
+getInfoFromFile();
+
+function getPostLinks() {
+	var results = []
+	request('https://www.reddit.com/r/leagueoflegends/', function (error, response, body) {
+		if(!error && response.statusCode == 200) {
+			$ = cheerio.load(body);
+			$('a.title').map(function(i, el) {
+				console.log($(el).attr("href"));
+				results.push($(el).attr("href"));
+			});
+			_.map(downloadUrl, results);
+		}
+	});
+}
+
+getPostLinks();
