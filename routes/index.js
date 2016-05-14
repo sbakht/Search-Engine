3cd58bdf516748ef34c5f161e80f3d6898a41f42
Saad Bakht <rotk13@gmail.com>
2016-05-13 23:47:54 -0400

    show snippet from first to last occurence

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,25 @@ router.post('/', function(req, res) {
 
   var fwdIndex = _.reduce(occurences, {}, query);
 
-  res.render('index', { title: 'Express', forwardIndex: JSON.stringify(fwdIndex), idToTitles: idToTitles });
+  var positions = {};
+  var range = {};
+  for(var id in fwdIndex) {
+    positions[id] = [];
+    query.forEach(function(word) {
+      // positions.push(invertedIndex[word][id]);
+      positions[id] = _.concat(positions[id], _.map(parseInt, invertedIndex[word][id]))
+    });
+    var diff = function(a, b) { return a - b; };
+    positions[id] = _.sort(diff, positions[id]);
+    console.log(positions[id])
+    var min = _.head(positions[id]);
+    var max = _.last(positions[id]);
+    console.log(max, min)
+    console.log(max - min)
+    range[id] = _.take(max-min+1, _.drop(min, fwdIndex[id]));
+  }
+
+  res.render('index', { title: 'Express', forwardIndex: JSON.stringify(fwdIndex), idToTitles: idToTitles , range: JSON.stringify(range)});
 
 });
 
